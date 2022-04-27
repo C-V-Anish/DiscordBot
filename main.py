c77c71720b065c70bba6a0c9b8e5b27bce383c2c
C.V. Anish <>
2022-04-27 19:50:03 +0000

    Added the encouragement functionality

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import discord
 import os
 import requests
 import json
+import random
 # Instance of a client-Connection to discord
 client = discord.Client()
 
+sad_words= ["sad","depressed","unhappy","angry","miserable","depressing"]
+
+encouragements=["Cheer Up!","You are too good","Be Motivated!"]
 def get_quote():
   response=requests.get("https://zenquotes.io/api/random")
   json_data=json.loads(response.text)
@@ -22,9 +26,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    msg = message.content
+
     if message.content.startswith('$inspire'):
       quote=get_quote()
       await message.channel.send(quote)
 
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(encouragements))
 
 client.run(os.getenv('TOKEN'))
