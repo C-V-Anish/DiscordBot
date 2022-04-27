e0a2bd69dfff01ff0a1669ff5874083ce66a326f
C-V-Anish <mahadevrao77@gmail.com>
2022-04-27 17:38:53 +0000

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# import packages
+import discord
+import os
+import requests
+import json
+# Instance of a client-Connection to discord
+client = discord.Client()
+
+def get_quote():
+  response=requests.get("https://zenquotes.io/api/random")
+  json_data=json.loads(response.text)
+  quote="''"+json_data[0]['q']+"''"+"-"+json_data[0]['a']
+  return(quote)
+# register an event
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$inspire'):
+      quote=get_quote()
+      await message.channel.send(quote)
+
+
+client.run(os.getenv('TOKEN'))
